f697d28df7c344a2017043cf68e8c84f705344b0
jn19392 <60779279+jn19392@users.noreply.github.com>
2023-03-31 11:09:48 +0100

    panic

--- agent_classes.py
+++ agent_classes.py
@@ -5,15 +5,25 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as ani
 from scipy.spatial import distance_matrix
 from scipy.ndimage import binary_dilation
-from tqdm import tqdm
-#%%    
+
+
+
+#%matplotlib notebook
+
+#%%
+class Agent():
+    def __init__(self, position):
+        self.position = position
+        self.escaped = False
+        self.panic = False
+    
 class Map():
-    def __init__(self, map_shape, num_agents, num_obstacles, num_exits, max_runs = 10, ta = 1):
-        self.map_shape = map_shape 
-        self.map = np.zeros(self.map_shape) # Create array of the map
+    def __init__(self, map_shape, num_agents, num_obstacles, num_exits, max_runs = 10):
+        self.map_shape = map_shape
+        self.map = np.zeros(self.map_shape)
+        self.ta = 0.5
         self.run = 0
         self.max_runs = max_runs
-        self.ta = ta # Acceleration rate coefficient 
         
         # Make obstacles
         self.obstacle_bool = np.zeros(np.array(map_shape), dtype=bool)
@@ -21,8 +31,6 @@ class Map():
         while num_obstacles_to_spawn != 0:
             self.obstacle_bool[np.unravel_index(np.random.choice(np.linspace(0, self.map.size, self.map.size, endpoint=False, dtype = int), num_obstacles_to_spawn), self.map.shape)] = 1
             num_obstacles_to_spawn = num_obstacles - np.sum(self.obstacle_bool.astype(int))
-        
-        # Make all the edges of the map obstacles
         edges = np.zeros((2*np.sum(np.array(map_shape))-2, 2))
         edges[:map_shape[0]*2 +4, 0] = np.repeat(np.array([np.linspace(-1, map_shape[1], map_shape[1]+2)]).T, 2, axis= 1).T.flatten()
         edges[:map_shape[0]*2 +4, 1] = np.repeat(np.array([-1, map_shape[1]]), (map_shape[1] + 2))
@@ -31,7 +39,7 @@ class Map():
         self.obstacle_positions = np.concatenate((np.argwhere(self.obstacle_bool == True), edges))
         
         # Make exits
-        self.exit_bool = np.zeros(map_shape, dtype= bool) 
+        self.exit_bool = np.zeros(map_shape, dtype= bool)
         exit_loc = np.zeros((num_exits, 2), dtype=int)
         i = 0 
         while i < num_exits:
@@ -43,38 +51,40 @@ class Map():
         
         # Spawn Agents
         self.num_agents = num_agents
-        self.agent_in = np.ones(num_agents, int) # An agent in, has not yet reached the exit
+        self.agent_in = np.ones(num_agents, int)
         self.agent_bool = np.zeros(map_shape, dtype= bool)
         num_agents_to_spawn = num_agents
+        self.agent_v = np.zeros((num_agents, 2))
         self.agent_positions = np.zeros((num_agents, 2, self.max_runs))
         while num_agents_to_spawn != 0:
             self.agent_bool[np.random.randint(0, map_shape[0], num_agents_to_spawn), np.random.randint(0, map_shape[1], num_agents_to_spawn)] = True
             self.agent_bool[np.logical_or(self.obstacle_bool, self.exit_bool)] = False
             num_agents_to_spawn = num_agents - np.sum(self.agent_bool.astype(int))
         self.agent_positions[:, :, 0] = np.argwhere(self.agent_bool== True)
+        self.calm = np.zeros((num_agents, max_runs))
         
         
     def update(self):
         if self.run < self.max_runs - 1:
             agent_positions = self.agent_positions[:, :, self.run]
             
-            # Calculate the direction to the closest exit for each agent
             dist_to_exit = distance_matrix(agent_positions, self.exit_positions)
             agent_exits = np.argmin(dist_to_exit, 1)
             exit_force = np.subtract(self.exit_positions[agent_exits], agent_positions)
             exit_force = np.divide(exit_force, np.array([np.linalg.norm(exit_force, axis =1)]).T)
-            
-            #Check whether an agent has finished 
             self.agent_in[np.min(dist_to_exit, axis= 1)<1] = 0
-            
-            '''
-            Calculate the force between agents
-                d = the distance between agents
-                dx and dy are the directions that the agent forces act in
-                agent_force is calculated and then split into directional components
-            '''
-            d = distance_matrix(agent_positions, agent_positions)
+            d = distance_matrix(agent_positions, agent_positions) * np.array([self.agent_in]).T
             d[np.isnan(d)] = 0
+            
+            # Calmness of the agents is based on the n closest agents
+            close_agents = 5
+            calm = np.ones(num_agents)
+            calm[np.any((d < 1) & (d>0), axis=1)] = np.sum(np.sort(d, axis=1)[:, :close_agents], axis=1)[np.any((d < 1) & (d>0), axis=1)]/close_agents
+            calm[calm < 0.5] = 0.5
+            self.calm[:, self.run] = calm
+            print(np.sum(calm)/self.num_agents)
+            
+            # dx and dy are the directions that the agent forces act in
             dx = np.subtract(agent_positions[:, 0], np.array([agent_positions[:, 0]]).T)/d
             dy = np.subtract(agent_positions[:, 1], np.array([agent_positions[:, 1]]).T)/d
             dx[np.isnan(dx)] = 0
@@ -85,7 +95,6 @@ class Map():
             agent_force_y = np.sum(np.multiply(agent_force, dy), axis = 1)
             agent_force_dir = np.concatenate((np.array([agent_force_x]).T, np.array([agent_force_y]).T), axis = 1)
             
-            # Calculate force between agents and obstacles
             do = distance_matrix(agent_positions, self.obstacle_positions)
             dox = np.subtract(np.array([agent_positions[:, 0]]).T, np.array([self.obstacle_positions[:, 0]]))/do
             doy = np.subtract(np.array([agent_positions[:, 1]]).T, np.array([self.obstacle_positions[:, 1]]))/do
@@ -97,19 +106,18 @@ class Map():
             obstacle_force_y = np.sum(np.multiply(obstacle_force, doy), axis = 1)
             obstacle_force_dir = np.concatenate((np.array([obstacle_force_x]).T, np.array([obstacle_force_y]).T), axis = 1)
             
-            # Calculate the total force on the agents
-            total_force = exit_force + agent_force_dir + obstacle_force_dir
+            total_force = (exit_force + agent_force_dir + obstacle_force_dir)* np.array([calm]).T
             agent_move = np.multiply(total_force/np.array([np.linalg.norm(total_force, axis = 1)]).T, np.array([self.agent_in]).T)
             agent_move[np.isnan(agent_move)] = 0
             agent_positions = np.add(agent_positions, agent_move)
+            #agent_positions[agent_positions < 0] = 0
+            #agent_positions[agent_positions >= map_shape[0]] = map_shape[0] - 1
             self.run += 1
             self.agent_exits = agent_exits
+            self.agent_v = self.agent_positions[:, :, self.run] - self.agent_positions[:, :, self.run- 1]
             self.agent_positions[:, :, self.run] = agent_positions
         
     def run_map(self):
-        '''
-        Runs the map till all the agents have finished or a max num of runs
-        '''
         while self.run < self.max_runs -1 and self.agent_in.any():
             self.update()
         self.map_metrics()
@@ -120,10 +128,27 @@ class Map():
         X = self.agent_positions[:, 0, :]
         Y = self.agent_positions[:, 1, :]
         dist_out = np.sum(((-np.diff(X))**2 + (-np.diff(Y))**2)**0.5)
-        print('A heauristic for the minimum distance: ', min_dist_out, 
-              '\n Total distance travelled: ', dist_out)
+        print(min_dist_out, dist_out)
         
     def draw_map(self):
+        # Animation plot
+        fig1, ax = plt.subplots()
+        ax.scatter(self.exit_positions[:, 0], self.exit_positions[:, 1], color = 'b', label = 'Exit')
+        ax.scatter(self.obstacle_positions[:, 0], self.obstacle_positions[:, 1], color = 'k', label = 'Obstacles')
+        scat = ax.scatter(self.agent_positions[:, 0, 0], self.agent_positions[:, 1, 0],color = 'r', label = 'Agent')
+        ax.set(xlim=[0, self.map_shape[0]], ylim=[0, self.map_shape[1]])
+        ax.legend()
+        def update(frame):
+            x = self.agent_positions[:, 0, frame]
+            y = self.agent_positions[:, 1, frame]
+            data = np.stack([x, y]).T
+            scat.set_offsets(data)
+            return (scat)
+        animation = ani.FuncAnimation(fig=fig1, func=update, frames=self.run, interval=50, repeat = False)
+        plt.show()
+        
+        '''
+        # Whole route plot
         fig = plt.figure()
         self.agent_positions = self.agent_positions[:, :, :self.run]       
         plt.scatter(self.agent_positions[:, 0, 0], self.agent_positions[:, 1, 0],color = 'r', label = 'Agent')
@@ -135,17 +160,13 @@ class Map():
         plt.legend()
         plt.axis("square")
         plt.show()
+        '''
 
 map_shape = (20, 20)
 num_agents = 40
 num_obstacles = 100
-num_exits = 1
-max_runs = 200
-
-if map_shape[0] != map_shape[1]:
-    raise Exception('The map must be a square.')
-if max_runs < map_shape[0]:
-    raise Warning('The maximum number of runs may be too low.')
+num_exits = 2
+max_runs = 100
 if num_agents + num_exits + num_obstacles >= map_shape[0] * map_shape[1]:
     raise Exception('There are too many objects for the size of map.')
 
@@ -153,4 +174,5 @@ map1 = Map(map_shape, num_agents, num_obstacles, num_exits, max_runs)
 
 map1.run_map()
 
-# %%
+
+#%%
